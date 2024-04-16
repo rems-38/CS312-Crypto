15ec55c2a67bb2bf0619293afe90ccc4db7c30c0
rems-38 <rem.mazzone@orange.fr>
2024-04-16 09:36:25 +0200

    Rot13 encrypt implementation

--- src/fr/esisar/cs312/crypto/Rot13.java
+++ src/fr/esisar/cs312/crypto/Rot13.java
@@ -4,6 +4,18 @@ public class Rot13 implements Algo {
 
     @Override
     public String encrypt(String text) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M')) { c += 13; }
+            else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z')) { c -= 13; }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String decrypt(String text) {
         return "Not implemented yet...";
     }
 
