0ec092488697dee3809dc7ad79426c03ccaedef4
Rem's 38 <57954491+rems-38@users.noreply.github.com>
2024-04-23 19:29:05 +0000

    Change of sh script to compile if not args provided (and not exec)

--- make.sh
+++ make.sh
@@ -2,4 +2,9 @@
 
 javac -cp src -d bin/ src/fr/esisar/cs312/crypto/* src/fr/esisar/cs312/crypto/algos/*
 jar -cvfe Main.jar fr.esisar.cs312.crypto.Main -C bin .
-java -jar Main.jar $@
+
+if [ "$#" -eq  "0" ]; then
+    exit 1
+else
+    java -jar Main.jar $@
+fi
