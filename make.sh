eb5cf96a9a81f5f8fa5d4e088034a0ed36cc957c
Rem's 38 <57954491+rems-38@users.noreply.github.com>
2024-04-23 18:58:49 +0000

    Change of sh script to handle new sub-package

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-javac -cp src -d bin/ src/fr/esisar/cs312/crypto/*
-jar -v -c -e fr.esisar.cs312.crypto.Main -f Main.jar -C bin .
+javac -cp src -d bin/ src/fr/esisar/cs312/crypto/* src/fr/esisar/cs312/crypto/algos/*
+jar -cvfe Main.jar fr.esisar.cs312.crypto.Main -C bin .
 java -jar Main.jar $@
