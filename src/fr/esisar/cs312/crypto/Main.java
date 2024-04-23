cfa1da5a9950a10f5a6a42f2bf52bc3373a356d2
Rem's 38 <57954491+rems-38@users.noreply.github.com>
2024-04-23 19:00:42 +0000

    Dynamic gestion of algos in Main

--- src/fr/esisar/cs312/crypto/Main.java
+++ src/fr/esisar/cs312/crypto/Main.java
@@ -1,9 +1,12 @@
 package fr.esisar.cs312.crypto;
 
+import java.util.Map;
+import java.util.HashMap;
 public class Main {
 
 	public static void main(String args[]) throws InvalidAlg, InvalidKey {
 		Message m = new Message();
+		Map<String, Algo> algos = getAlgo();
 		
 		if (args.length > 4) {
 			System.out.println("Usage: -e|-d alg text [args ...]");
@@ -17,10 +20,7 @@ public class Main {
 			System.exit(1);
 		}
 
-		if (args[1].equals("rot13")) { m.algo = new Rot13(); }
-		else if (args[1].equals("caesar")) { m.algo = new Caesar(); }
-		else if (args[1].equals("sub")) { m.algo = new Sub(); }
-		else if (args[1].equals("otp")) { m.algo = new Otp(); }
+		if (algos.containsKey(args[1])) { m.algo = algos.get(args[1]); }
 		else { throw new InvalidAlg("Invalid alg.\nUsage : -e|-d alg text [args ...]"); }
 
 		if (m.encrypt) { m.plaintext = args[2]; }
@@ -39,5 +39,25 @@ public class Main {
 			System.out.println(m.plaintext);
 		}
 		
+	}
+	private static Map<String, Algo> getAlgo() {
+		Map<String, Algo> algos = new HashMap<>();
+		String[] files = new java.io.File("src/fr/esisar/cs312/crypto/algos").list();
+		
+		for (String file : files) {
+			if (file.endsWith(".java")) { // on ne charge que les fichiers .java
+				String name = file.substring(0, file.length() - 5); // on enlève le .java
+				try {
+					Class<?> c = Class.forName("fr.esisar.cs312.crypto.algos." + name); // on charge la classe associée
+					Algo algoInstance = (Algo) c.getDeclaredConstructor().newInstance(); // on instancie la classe
+					algos.put(name.toLowerCase(), algoInstance); // on ajoute l'algo à la liste
+				} catch (Exception e) {
+					System.out.println("Error while loading " + name);
+				}
+			}
+		}
+
+		
+		return algos;
 	}
 }
